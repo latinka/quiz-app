14356fb8589574dcda9f396abfe74a56be5def0a
latinka <l_doychinova@yahoo.com>
2015-06-10 23:47:08 +0300

    CSS changes

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var allQuestions = [
 	{
 		question		: "On what date is Harry's birthday?",
 		image     		: "img/birthday-cake.png",
+				
+						
 														
 		choices 		: [
 							"August 15",
@@ -70,7 +72,7 @@ var allQuestions = [
 							], 
 		correct 		: 3, 
 		explanationc	: "Correct! Professor Minerva McGonagall is a teacher of Transfiguration, Head of House Gryffindor, registered animagus and later on- Headmistress of Hogwarts!",
-		explanationw 	: "School isn't just about trips to Hogsmeade. Professor Minerva McGonagall is a teacher or Transfiguration, Head of House Gryffindor, registered animagus and later on- Headmistress of Hogwarts!", 
+		explanationw 	: "School isn't just about trips to Hogsmeade! Professor Minerva McGonagall is a teacher or Transfiguration, Head of House Gryffindor, registered animagus and later on- Headmistress of Hogwarts!", 
 		hint			: "'This is some of the most complex and dangerous magic you will learn at Hogwarts. Anyone messing around in my class will leave and not come back. You have been warned.'", 
 	},
 	{
@@ -85,7 +87,7 @@ var allQuestions = [
 							], 
 		correct 		: 2, 
 		explanationc	: "Correct! The 422nd Quidditch World Cup was held in England. Its official sponsors were Butterbeer, Pumpkin Juice, Gringotts Wizarding Bank and Nimbus 2001. The final was between Ireland and Bulgaria. Ireland won.",
-		explanationw 	: "Maybe you're more into Muggle sports. The 422nd Quidditch World Cup was held in England. Its official sponsors were Butterbeer, Pumpkin juice, Gringotts Wizarding bank and Nimbus 2001. The final was between Ireland and Bulgaria. Ireland won.", 
+		explanationw 	: "Maybe you're more into Muggle sports! The 422nd Quidditch World Cup was held in England. Its official sponsors were Butterbeer, Pumpkin juice, Gringotts Wizarding bank and Nimbus 2001. The final was between Ireland and Bulgaria. Ireland won.", 
 		hint			: "Victor Krum caught the Snitch! Brackium Emendo!", 
 	},
 	{
@@ -153,74 +155,71 @@ var allQuestions = [
 
 $(document).ready(function() {
 
-	$(".question").hide();
-	$("#qimage").hide();
-	$(".answers").hide();
-	$(".hint").hide();
-	$("#modal").hide();
-	$(".next").hide();
-	$(".explanation").hide();
-	$(".blur").hide();
-	$("#overlay").hide();
+	$(".question, #qimage, .answers, .hint, #modal, .next, .feedback, .results, .explanation, .blur, #overlay, #restart").hide();
 
+//Load the picture, question and answers after clicking the "Start" button.
 $("#start").on("click", function(){
-	$("#simage").hide();
-	$("h1").hide();
-	$("#start").hide();
-	$("#count").show();
-	$(".question").show();
-	$("#qimage").show();
-	$(".answers").show();
-	$(".hint").show();
+	$("#simage, h1, #start").hide();
+	$("#count, .question, #qimage, .answers, .hint").show();
 	currentQuestion++;
 	counter();
 	askQuestion();
 });
 
+//Show the hint overlay after clicking on "Show Hint".
 $(".hint").on("click", function(){
 	$("#count").hide();
-	$("#overlay").fadeIn(500);
-	$("#modal").fadeIn(500);
+	$("#overlay").fadeIn(700);
+	$("#modal").fadeIn(700);
 });
 
+//Close the hint overlay by clicking the "X" in the upper right corner.
 $(".close").on("click", function(){
 	$("#count").show();
 	$("#modal").fadeOut(500);
 	$("#overlay").fadeOut(500);
 });
 
+//Close the hint overlay by clicking outside of it.
 $("#overlay").on("click", function(){
 	$("#count").show();
 	$("#modal").fadeOut(500);
 	$("#overlay").fadeOut(500);
 });
 
-
-$("#answers").on("click", function(){
+//Show explanation and check answer for correctness after the user picks an answeer.
+$("#answers").on("click", "input:not([readonly])", function(){
 	$(".hint").hide();
-	$(".next").fadeIn(500);
 	$(".explanation").fadeIn(500);
 	checkAnswer();
+	if (currentQuestion >= allQuestions.length - 1) {
+		$(".results").fadeIn(500);
+		$(".next").hide();
+	}
+	else {
+		$(".next").fadeIn(500);
+	}
 });
 
 var currentQuestion = -1;
 var score = 0;
 
+//Function to show how far user has progressed inthe feedback-t.
 var counter = function(){
 	$("#count").text("Question " + (currentQuestion+1) + " of 10");
 };
 
 
-
+//Function to load the questions, answers, images and hints for every question.
 function askQuestion(){
-		var choices = allQuestions[currentQuestion].choices;
-		$(".ask").text(allQuestions[currentQuestion].question);
-		
+	$(".ask").text(allQuestions[currentQuestion].question);
+	var choices = allQuestions[currentQuestion].choices;
+	
 		for (var i = 0; i < choices.length; i++){
 			var radio_button = $("<input>")
 				.attr("id", "choices-" + i)
 				.attr("type", "radio")
-				.attr("name", "choices")
+				.attr("name", "options")
 				.attr("value", i)
 				.attr("checked", i === this.user_choice)
 				.appendTo("#answers");
@@ -233,28 +232,81 @@ function askQuestion(){
 
 	$(".hint-text").text(allQuestions[currentQuestion].hint);
 	$("#qimage").attr("src", allQuestions[currentQuestion].image);
+}
 
-};
-
+//Function to check if answer is correct.
 function checkAnswer(){
-	var userAnswer = $("input[type=radio]:checked").val();
-	if (userAnswer === allQuestions[currentQuestion].correct) {
-		$(".explanation").text(allQuestions[currentQuestion].explanationc)
+	$("input[type = 'radio']").prop("readonly", true);
+	var userAnswer = $("input[type = 'radio']:checked").val();
+	var correctAnswer = allQuestions[currentQuestion].correct;
+	if (userAnswer == correctAnswer) {
+		$(".explanation").text(allQuestions[currentQuestion].explanationc);
+		$("input[type = 'radio']:checked + label").addClass("right");
+		$("input[type = 'radio']:not(:checked) + label").addClass("answered");
 		score++;
+		console.log(score);
 	}
 	else {
 		$(".explanation").text(allQuestions[currentQuestion].explanationw);
-	};
+		$("input[type = 'radio']:checked + label").addClass("wrong");
+		$("input[type = 'radio']:not(:checked) + label").addClass("answered");
+	}
 };
 
+//Function to get results.
+function getResults(){
+	$(".correct-count").text("You got " + score + " out of 10 correct!")
+	if (score >= 9) {
+		$(".headline").text("You are a true Potterhead!");
+		$(".feedback-text").text("You love all things Harry Potter and stil haven't given up hope that one day you will find your very own Hogwarts letter in the mail.");
+	}
+	else if (score >= 7) {
+		$(".headline").text("You know your Harry Potter facts!");
+		$(".feedback-text").text("Although some minor details might have escaped your memory, you're still part of the worldwide fandom!");
+	}
+	else if (score >= 5) {
+		$(".headline").text("You know the basics!");
+		$(".feedback-text").text("Maybe it's been a while since you read the books but you remember enough to not feel awkward when someone mentions Harry Potter.");
+	}
+	else if (score >= 3) {
+		$(".headline").text("You're a bit rusty!");
+		$(".feedback-text").text("Your Harry Potter knowledge has waned over the years. Maybe it's time to read those books again!");
+	}
+	else if (score >= 1) {
+		$(".headline").text("You are a Muggle!");
+		$(".feedback-text").text("You need to catch up on your reading or at least see the movies again so you can bring back some magic into your life!");
+	}
+	else {
+		$(".headline").text("Magic is not your thing!");
+		$(".feedback-text").text("Have you ever heard the name Harry Potter?");
+	}
+};
+
+//Clear feedback and ask question when "Next" is clicked.
 $(".next").on("click", function(){
 	currentQuestion++;
 	counter();
-	$(".next").hide();
-	$(".explanation").hide();
+	$(".next, .explanation").hide();
 	$(".hint").show();
-	$("#answers").empty();
+	$("label, input[type='radio']").remove();
 	askQuestion();
+});
+
+//Clear everything and show results upon clicking "Results".
+$(".results").on("click", function(){
+	$("#count, .hint, #answers, #qimage, .explanation, .results").hide();
+	$("#restart, .feedback").show();
+	getResults();
+});
+
+//Reload page function.
+function reloadPage(){
+	location.reload();
+};
+
+//Reload the page when user clicks "Play Again".
+$("#restart").on("click", function(){
+	reloadPage();
 });
 
 });
